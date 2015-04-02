1aabf1c6d6621a250ee7bca07f35373bae9999ec
Yi-Ju Chung <yi-ju.chung@bazaarvoice.com>
2015-04-02 00:24:07 -0700

    solve the flatten bt

--- src/FlattenBinaryTreetoLinkedList.java
+++ src/FlattenBinaryTreetoLinkedList.java
@@ -1,6 +1,33 @@
 public class FlattenBinaryTreetoLinkedList {
     static public void flatten(TreeNode root) {
+        flattenHelper(root);
+    }
 
+    static public TreeNode flattenHelper(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+
+        if (root.left == null && root.right == null) {
+            return root;
+        } else if (root.right == null) {
+            // have left child
+            TreeNode lastNode = flattenHelper(root.left);
+            root.right = root.left;
+            root.left = null;
+            return lastNode;
+        } else if (root.left == null){
+            // have right child
+            return flattenHelper(root.right);
+        } else {
+            // have both
+            TreeNode oriRight = root.right;
+            TreeNode lastNode = flattenHelper(root.left);
+            root.right = root.left;
+            root.left = null;
+            lastNode.right = oriRight;
+            return flattenHelper(root.right);
+        }
     }
 
     public static void main(String [ ] args) {
